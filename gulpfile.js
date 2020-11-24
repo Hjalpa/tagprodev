9959aabb133dd2393488ff1d894afb9f753f039d
anom <gitlab@danielhellier.com>
2020-11-24 15:56:58 +0000

    updates

---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+const gulp 				= require('gulp')
+const watch 			= require('gulp-watch')
+const stylus 			= require('gulp-stylus')
+const cleanCSS 			= require('gulp-clean-css')
+const autoprefix		= require('gulp-autoprefixer')
+const concat 			= require('gulp-concat')
+const uglify 			= require('gulp-uglify-es').default
+const stripDebug 		= require('gulp-strip-debug')
+const size 				= require('gulp-size')
+const browserSync		= require('browser-sync')
+const reload 			= browserSync.reload
+const rsync 			= require('rsyncwrapper')
+
+
+//  browser sync & reload
+// --------------------------------------------------------------------------
+gulp.task('browser-sync', gulp.series(function(done) {
+	browserSync({
+		proxy: "caloriemap",
+		// move JS inserts to head
+		snippetOptions: {
+			rule: {
+				match: /<\/head>/i,
+				fn: function (snippet, match) {
+					return snippet + match;
+				}
+			}
+		},
+		notify: false
+	})
+
+	done()
+}))
+
+
+//  js app
+// --------------------------------------------------------------------------
+gulp.task('js', gulp.series(function(done) {
+	gulp.src([
+			'./src/js/vendors/*.js',
+			'./src/js/util.js',
+			'./src/js/init.js',
+			'./src/js/*.js'
+		])
+		.pipe(concat('init.js'))
+		// .pipe(uglify())
+		.pipe(size(
+			{'title':'init.js'}
+		))
+		.pipe(gulp.dest('./public/'))
+
+	done()
+}))
+
+
+
+
+//  css minification
+// --------------------------------------------------------------------------
+gulp.task('css', gulp.series(function(done) {
+	gulp.src(['./src/css/main.styl', './src/css/*.styl'])
+		.pipe(stylus())
+		.pipe(concat('init.css'))
+		.pipe(autoprefix('last 2 versions'))
+	    .pipe(cleanCSS())
+		.pipe(size(
+			{'title':'init.css'}
+		))
+		.pipe(gulp.dest('./public/'))
+		.pipe(reload({stream:true}))
+
+	done()
+}))
+
+
+//  watch for edits
+// --------------------------------------------------------------------------
+gulp.task('watch', gulp.series(gulp.parallel('browser-sync'), function(done) {
+	gulp.watch('./src/css/*.styl', gulp.series('css'))
+	gulp.watch(['./src/js/*.js', './src/js/vendors/*.js'], gulp.series('js'))
+
+	done()
+}))
