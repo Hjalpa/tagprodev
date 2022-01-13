3be38797299784d0be2f87c1dfe787fee1a03403
anom <gitlab@danielhellier.com>
2022-01-13 16:03:17 +0000

    test

--- models/superleague/stats.js
+++ models/superleague/stats.js
@@ -9,7 +9,7 @@ let init = async (req, res) => {
 		nav: {
 			primary: 'superleague',
 			secondary: 'stats',
-			tertiary: 'totals',
+			tertiary: 'summary',
 		},
 		stats: await getData()
 	}
@@ -21,30 +21,34 @@ let init = async (req, res) => {
 async function getData(filters) {
 	let sql = `
 		SELECT
-			RANK() OVER (
-				ORDER BY sum(cap) + sum(assist) DESC
-			) rank,
-
+			COALESCE(team.acronym, 'SUB') as acronym,
+			COALESCE(team.color, '#404040') as color,
 			player.name as player,
+
+			TO_CHAR( sum(play_time) * interval '1 sec', 'hh24:mi:ss') as time,
+
+			SUM(cap)+SUM(assist) as points,
+			SUM(cap) as caps,
+			SUM(assist) as assists,
 
 			SUM(tag) as tags,
 			SUM(pop) as pops,
 			SUM(grab) as grabs,
 			SUM(drop) as drops,
 			TO_CHAR( sum(hold) * interval '1 sec', 'hh24:mi:ss') as hold,
-			SUM(cap) as caps,
 			TO_CHAR( sum(prevent) * interval '1 sec', 'hh24:mi:ss') as prevent,
-			SUM(return) as returns,
+			SUM(return) as returns
 
-			TO_CHAR( sum(play_time) * interval '1 sec', 'hh24:mi:ss') as time,
-			count(*) as games
 
 		FROM playergame
 		LEFT JOIN player ON player.id = playergame.playerid
+		LEFT JOIN seasonplayer ON player.id = seasonplayer.playerid
+		LEFT JOIN seasonteam ON seasonteam.id = seasonplayer.seasonteamid
+		LEFT JOIN team ON seasonteam.teamid = team.id
 		LEFT JOIN game ON game.id = playergame.gameid
 		WHERE game.seasonid = 5
-		GROUP BY player.name
-		ORDER BY rank ASC
+		GROUP BY player.name, team.color, team.acronym
+		ORDER BY team.acronym ASC
 	`
 	let data = await db.select(sql, [], 'all')
 
