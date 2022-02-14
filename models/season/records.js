c7e3e3afcbc8cca609890c0284e085efc0bf1f69
anom <gitlab@danielhellier.com>
2022-02-14 12:08:08 +0000

    test

--- models/season/records.js
+++ models/season/records.js
@@ -21,7 +21,154 @@ let init = async (req, res) => {
 					page: 'records'
 				}
 			},
-			records: {
+			records: await getRecords(filters, req.mode)
+		}
+
+		res.render('superleague-records', data);
+	} catch(e) {
+		res.status(400).json({error: e})
+	}
+}
+
+async function getData(filters, select) {
+	let ascending = (filters.ascending === true) ? 'ASC' : 'DESC'
+	let percentage = (filters.percentage) ? " || '%' " : ''
+
+	let raw = await db.select(`
+		SELECT
+			RANK() OVER (
+				ORDER BY
+					${select} ${ascending}
+			) rank,
+			player.name as player,
+			COALESCE(team.acronym, 'SUB') as acronym,
+			COALESCE(team.color, '#404040') as color,
+			map.name as map,
+			TO_CHAR(date, 'DD Mon') as date,
+			euid,
+			${select} ${percentage} as value
+
+		FROM playergame
+		LEFT JOIN player ON player.id = playergame.playerid
+		LEFT JOIN seasonplayer ON player.id = seasonplayer.playerid
+		LEFT JOIN seasonteam ON seasonteam.id = seasonplayer.seasonteamid
+		LEFT JOIN team ON seasonteam.teamid = team.id
+		LEFT JOIN game ON game.id = playergame.gameid
+		LEFT JOIN map ON map.id = game.mapid
+
+		WHERE gameid in (
+			SELECT game.id
+			FROM game
+			LEFT JOIN seasonschedule ON seasonschedule.gameid = game.id
+			WHERE gameid = game.id AND game.seasonid = $1 AND league = TRUE
+		) AND seasonteam.seasonid = $1
+		ORDER BY rank ASC, game.date ASC
+		LIMIT 10
+	`, [filters.seasonid], 'all')
+
+	return raw
+}
+
+async function getRecords(filters, mode) {
+	switch(mode) {
+		case 'ctf':
+			return {
+				points: {
+					title: 'Points',
+					data: await getData(filters, 'cap + assist'),
+				},
+				caps: {
+					title: 'Caps',
+					data: await getData(filters, 'cap'),
+				},
+				assists: {
+					title: 'Assists',
+					data: await getData(filters, 'assist'),
+				},
+				pups: {
+					title: 'Pups',
+					data: await getData(filters, 'pup_tp + pup_rb + pup_jj'),
+				},
+				tagpros: {
+					title: 'Tagpros',
+					data: await getData(filters, 'pup_tp'),
+				},
+				tags: {
+					title: 'Tags',
+					data: await getData(filters, 'tag'),
+				},
+				grabs: {
+					title: 'Grabs',
+					data: await getData(filters, 'grab'),
+				},
+				hold: {
+					title: 'Hold',
+					data: await getData(filters, `TO_CHAR(hold * interval '1 sec', 'mi:ss')`),
+				},
+				prevent: {
+					title: 'Prevent',
+					data: await getData(filters, `TO_CHAR(prevent * interval '1 sec', 'mi:ss')`),
+				},
+				returns: {
+					title: 'Returns',
+					data: await getData(filters, 'return'),
+				},
+				quickreturns: {
+					title: 'Quick Returns',
+					data: await getData(filters, 'quick_return'),
+				},
+				keyreturns: {
+					title: 'Key Returns',
+					data: await getData(filters, 'key_return'),
+				},
+				grabpercap: {
+					title: 'Cap / Grab',
+					data: await getData({...filters, ...{percentage: true}}, `
+						Round((cap::DECIMAL / grab::DECIMAL) * 100, 0)
+					`)
+				},
+				holdpergrab: {
+					title: 'Hold / Grab',
+					data: await getData(filters, `
+						Round(hold::DECIMAL / grab::DECIMAL, 2)
+					`)
+				},
+				holdpercap: {
+					title: 'Hold / Cap',
+					data: await getData({...filters, ...{having: true, ascending: true}}, `
+						Round(hold::DECIMAL / NULLIF(cap::DECIMAL, 0), 2)
+					`)
+				},
+				pupcaps: {
+					title: 'Pup Caps',
+					data: await getData(filters, 'cap_whilst_team_have_active_pup'),
+				},
+				kisses: {
+					title: 'Kisses',
+					data: await getData(filters, 'kiss'),
+				},
+				longhold: {
+					title: 'Long Hold',
+					data: await getData(filters, 'long_hold'),
+				},
+				tagpop: {
+					title: 'Tag / Pop',
+					data: await getData(filters, 'tag - pop'),
+				},
+				timedead: {
+					title: 'Time Dead',
+					data: await getData({...filters, ...{ascending: true}}, `
+						TO_CHAR((pop * 3) * interval '1 sec', 'mi:ss')
+					`),
+				},
+				score: {
+					title: 'Score',
+					data: await getData(filters, 'score'),
+				},
+			}
+		break;
+		case 'nf':
+			return {
 				points: {
 					title: 'Points',
 					data: await getData(filters, 'cap + assist'),
@@ -127,49 +274,6 @@ let init = async (req, res) => {
 					`),
 				},
 			}
-		}
-
-		res.render('superleague-records', data);
-	} catch(e) {
-		res.status(400).json({error: e})
+		break;
 	}
-}
-
-async function getData(filters, select) {
-	let ascending = (filters.ascending === true) ? 'ASC' : 'DESC'
-	let percentage = (filters.percentage) ? " || '%' " : ''
-
-	let raw = await db.select(`
-		SELECT
-			RANK() OVER (
-				ORDER BY
-					${select} ${ascending}
-			) rank,
-			player.name as player,
-			COALESCE(team.acronym, 'SUB') as acronym,
-			COALESCE(team.color, '#404040') as color,
-			map.name as map,
-			TO_CHAR(date, 'DD Mon') as date,
-			euid,
-			${select} ${percentage} as value
-
-		FROM playergame
-		LEFT JOIN player ON player.id = playergame.playerid
-		LEFT JOIN seasonplayer ON player.id = seasonplayer.playerid
-		LEFT JOIN seasonteam ON seasonteam.id = seasonplayer.seasonteamid
-		LEFT JOIN team ON seasonteam.teamid = team.id
-		LEFT JOIN game ON game.id = playergame.gameid
-		LEFT JOIN map ON map.id = game.mapid
-
-		WHERE gameid in (
-			SELECT game.id
-			FROM game
-			LEFT JOIN seasonschedule ON seasonschedule.gameid = game.id
-			WHERE gameid = game.id AND game.seasonid = $1 AND league = TRUE
-		) AND seasonteam.seasonid = $1
-		ORDER BY rank ASC, game.date ASC
-		LIMIT 10
-	`, [filters.seasonid], 'all')
-
-	return raw
 }
