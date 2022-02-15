9815dfcc6ea3eefece2f5d84c6129223b0c2ca66
anom <gitlab@danielhellier.com>
2022-02-15 10:25:28 +0000

    fix columns

--- src/js/init.js
+++ src/js/init.js
@@ -5,38 +5,7 @@ app.tablesort= (async () => {
 		await highlight()
 
 		const containerRef = document.querySelector('.player-stats')
-		if(document.querySelector('.player-stats [data-column="takeovers"]')) {
-			items = [...containerRef.querySelectorAll('.row.entry')].map(item=>({
-					mins : Number(item.querySelector('[data-column="mins"]').textContent),
-					caps: Number(item.querySelector('[data-column="caps"]').textContent),
-					assists: Number(item.querySelector('[data-column="assists"]').textContent),
-					poss: util.getValue(item.querySelector('[data-column="poss"]').textContent),
-					tags: Number(item.querySelector('[data-column="tags"]').textContent),
-					takeovers: Number(item.querySelector('[data-column="takeovers"]').textContent),
-					grabs: Number(item.querySelector('[data-column="grabs"]').textContent),
-					hold: util.getValue(item.querySelector('[data-column="hold"]').textContent),
-					chains: Number(item.querySelector('[data-column="chains"]').textContent),
-					prevent: util.getValue(item.querySelector('[data-column="prevent"]').textContent),
-					block: util.getValue(item.querySelector('[data-column="block"]').textContent),
-					pups: Number(item.querySelector('[data-column="pups"]').textContent),
-					itmRef : item,
-				}))
-		}
-		else {
-			items = [...containerRef.querySelectorAll('.row.entry')].map(item=>({
-					mins : Number(item.querySelector('[data-column="mins"]').textContent),
-					caps: Number(item.querySelector('[data-column="caps"]').textContent),
-					hold: util.getValue(item.querySelector('[data-column="hold"]').textContent),
-					grabs: Number(item.querySelector('[data-column="grabs"]').textContent),
-					assists: Number(item.querySelector('[data-column="assists"]').textContent),
-					prevent: util.getValue(item.querySelector('[data-column="prevent"]').textContent),
-					returns: Number(item.querySelector('[data-column="returns"]').textContent),
-					tags: Number(item.querySelector('[data-column="tags"]').textContent),
-					kd: Number(item.querySelector('[data-column="kd"]').textContent),
-					pups: Number(item.querySelector('[data-column="pups"]').textContent),
-					itmRef : item,
-				}))
-		}
+		items = [...containerRef.querySelectorAll('.row.entry')].map(item=>sortable(item))
 
 		document.querySelector('.row.head').addEventListener('click', (e) => {
 			let col = util.findParentBySelector(e.target, '[data-column]')
@@ -60,8 +29,6 @@ app.tablesort= (async () => {
 	}
 
 	async function highlight() {
-
-		// each row
 		let data = {}
 		document.querySelectorAll('.row.entry').forEach((row) => {
 
@@ -87,9 +54,18 @@ app.tablesort= (async () => {
 				}
 
 			})
+		})
+	}
 
+	function sortable(row) {
+		let data = {itmRef:row}
+		row.querySelectorAll('[data-column]').forEach((elm) => {
+			let name = elm.dataset.column
+			data[name] = util.getValue(elm.innerText)
 		})
 
+		return data
 	}
+
 })
 app.tablesort()
