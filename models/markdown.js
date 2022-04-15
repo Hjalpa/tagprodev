438f1e3221e85589049a7323bb6a5248a42649bb
anom <gitlab@danielhellier.com>
2022-04-15 14:43:17 +0000

    test

--- models/markdown.js
+++ models/markdown.js
@@ -1,16 +1,10 @@
-const db = require ('../lib/db')
-const util = require ('../lib/util')
-
 const matter = require('gray-matter')
 const MarkdownIt = require('markdown-it')
 const md = new MarkdownIt();
 
-module.exports.init = async (req, res, filename) => await init(req, res, filename)
-let init = async (req, res, filename) => {
+module.exports.init = async (req, res, filename) => {
 	try {
-		let path = `./pages/${filename}.md`
-		let markdown = await matter.read(path)
-
+		let markdown = await matter.read(`./pages/${filename}.md`)
 		let data = {
 			config: {
 				title: markdown.data.title,
