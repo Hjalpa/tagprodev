74449cb6fa9fc2f4ec47c755d94250868bf2a5b3
anom <gitlab@danielhellier.com>
2023-03-07 15:01:37 +0000

    Update spy_update.js

--- tasks/spy_update.js
+++ tasks/spy_update.js
@@ -9,7 +9,7 @@ init.call = async () => {
 	let spies = await db.query("SELECT tpid, name FROM spy ORDER BY lastseendate DESC", 'all')
 	for(let player in spies) {
 		let p = spies[player]
-		await axios.post(`https://tagpro.dev/api/spy/update`, {
+		await axios.post(`https://tagpro-rp2xu.ondigitalocean.app/api/spy/update`, {
 			tpid: p.tpid
 		})
 
