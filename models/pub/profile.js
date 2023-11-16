c32a184715e46ace96c7ae879a27d2adf703dfc7
anom <gitlab@hellier.dev>
2023-11-16 12:58:03 +0000

    get games for profile api

--- models/pub/profile.js
+++ models/pub/profile.js
@@ -3,14 +3,24 @@ const util = require ('../../lib/util')
 
 module.exports.init = async (req, res) => {
 	try {
-		let data = await getProfileData(req.params.profileID)
-		res.json(data)
+		let profileID = req.params.profileID
+		res.json({
+			games: await getGames(profileID),
+			stats: {
+				day: await getStats(profileID, 'day'),
+				week: await getStats(profileID, 'week'),
+				month: await getStats(profileID, 'month'),
+				all: await getStats(profileID, 'all'),
+			}
+		})
 	} catch(e) {
 		res.status(400).send({error: e})
 	}
 }
 
-async function getProfileData(profileID) {
+async function getStats(profileID, datePeriod) {
+	let dateFilter = (datePeriod === 'all' ? '' : ` AND tp_playergame.datetime >= NOW() - interval '1 ${datePeriod}'`);
+
 	let raw = await db.select(`
 		SELECT
 			COUNT(*)::REAL as games,
@@ -22,15 +32,35 @@ async function getProfileData(profileID) {
 			SUM(cap_team_for)::REAL as CF,
 			SUM(cap_team_against)::REAL as CA,
 			SUM(cap_team_for - cap_team_against)::REAL as CD,
-			TO_CHAR(SUM(duration) * interval '1 sec', 'hh24:mi:ss') as timeplayed,
-			ROUND(p.openskill::decimal, 2)::REAL as openskill
+			TO_CHAR(SUM(duration) * interval '1 sec', 'hh24:mi:ss') as timeplayed
 
 		FROM tp_playergame
 		LEFT JOIN tp_player as p ON p.id = tp_playergame.playerid
-		WHERE p.tpid = $1
-		GROUP BY p.id, p.openskill
+		WHERE p.tpid = $1 ${dateFilter}
+		GROUP BY p.id
 		LIMIT 1
 	`, [profileID], 'row')
+
+	return raw
+}
+
+async function getGames(profileID) {
+	let raw = await db.select(`
+		SELECT
+			pg.datetime,
+			pg.duration,
+			pg.finished,
+			pg.team,
+			pg.winner,
+			pg.cap_team_for,
+			pg.cap_team_against,
+			pg.openskill
+		FROM tp_playergame as pg
+		LEFT JOIN tp_player as p ON p.id = pg.playerid
+		WHERE p.tpid = $1
+		ORDER BY datetime ASC
+		LIMIT 1000;
+	`, [profileID], 'all')
 
 	return raw
 }
