c38bec8d82592aae6a75dfdf3f595cba67b83c0d
anom <gitlab@hellier.dev>
2023-11-16 09:30:40 +0000

    add cache for leaderboard API

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,25 @@
 const express = require('express')
 const router = express.Router()
+const cache = require('memory-cache')
+
+let memCache = new cache.Cache()
+let cacheMiddleware = duration => {
+	return (req, res, next) => {
+		let key =  '__express__' + req.originalUrl || req.url
+		let cacheContent = memCache.get(key)
+		if(cacheContent){
+			res.send(cacheContent)
+			return
+		}else{
+			res.sendResponse = res.send
+			res.send = body => {
+				memCache.put(key,body,(duration*1000) * 24)
+				res.sendResponse(body)
+			}
+			next()
+		}
+	}
+}
 
 const imp = require('../models/import')
 router.post('/import', (req, res) => imp.game(req, res))
@@ -15,7 +35,7 @@ router.post('/spy/update', (req, res) => spy.update(req, res))
 
 // pub data
 router.post('/pub/import', (req, res) => require('../models/pub/import').import(req, res))
-router.get('/pub/leaderboard', (req, res) => require('../models/pub/leaderboard').init(req, res))
-router.get('/pub/profile', (req, res) => require('../models/pub/profile').init(req, res))
+router.get('/pub/leaderboard', cacheMiddleware(400), (req, res) => require('../models/pub/leaderboard').init(req, res))
+router.get('/pub/profile', cacheMiddleware(400), (req, res) => require('../models/pub/profile').init(req, res))
 
 module.exports = router
