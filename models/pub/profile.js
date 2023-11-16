dee4c3ed7095485bf647e3fc14da0d74a8689dc9
anom <gitlab@hellier.dev>
2023-11-16 10:47:56 +0000

    profile api

--- models/pub/profile.js
+++ models/pub/profile.js
@@ -3,58 +3,20 @@ const util = require ('../../lib/util')
 
 module.exports.init = async (req, res) => {
 	try {
-		let data = await getData(profileID)
+		let data = await getProfileData(req.params.profileID)
 		res.json(data)
 	} catch(e) {
 		res.status(400).send({error: e})
 	}
 }
 
-async function getData(datePeriod) {
+async function getProfileData(profileID) {
 	let raw = await db.select(`
 		SELECT
-			RANK() OVER (
-				ORDER BY p.openskill::real DESC
-			)::real rank,
-
-			p.name as name,
-			p.tpid as profile,
-
-			COUNT(*)::real as games,
-			COUNT(*) filter (WHERE tp_playergame.winner = true)::real as wins,
-			COUNT(*) filter (WHERE tp_playergame.winner = false)::real as losses,
-
-			ROUND(COUNT(*) FILTER (WHERE tp_playergame.winner = true) * 100.0 / COUNT(*), 2)::REAL AS winrate,
-
-			SUM(cap_team_for)::real as CF,
-			SUM(cap_team_against)::real as CA,
-			SUM(cap_team_for - cap_team_against)::real as CD,
-
-			array(
-				SELECT jsonb_build_object(
-					'uuid', tp_game.uuid,
-					'winner', tp_playergame.winner
-				)
-				FROM tp_playergame
-				LEFT JOIN tp_game on tp_game.id = tp_playergame.gameid
-				WHERE tp_playergame.playerid = p.id
-				ORDER BY tp_playergame.datetime DESC
-				LIMIT 10
-			) as form,
-
-			TO_CHAR(SUM(duration) * interval '1 sec', 'hh24:mi:ss') as timeplayed,
-			MAX(tp_playergame.datetime) as lastseen,
-
-			ROUND(p.openskill::decimal, 2)::real  as openskill,
-			(SELECT flair from tp_playergame as tppg where tppg.playerid = p.id ORDER by id DESC LIMIT 1) as flair
-
-		FROM tp_playergame
-		LEFT JOIN tp_player as p ON p.id = tp_playergame.playerid
-		WHERE p.tpid is not null
-		GROUP BY p.name, p.id, p.openskill, profile
-		ORDER BY rank ASC, cd DESC, winrate DESC, wins DESC
-		LIMIT 100
-	`, [], 'all')
+			openskill as skill
+		FROM tp_player
+		WHERE tp_player.tpid = $1
+	`, [profileID], 'row')
 
 	return raw
 }
--- routes/api.js
+++ routes/api.js
@@ -36,6 +36,6 @@ router.post('/spy/update', (req, res) => spy.update(req, res))
 // pub data
 router.post('/pub/import', (req, res) => require('../models/pub/import').import(req, res))
 router.get('/pub/leaderboard', cacheMiddleware(400), (req, res) => require('../models/pub/leaderboard').init(req, res))
-router.get('/pub/profile', cacheMiddleware(400), (req, res) => require('../models/pub/profile').init(req, res))
+router.get('/pub/profile/:profileID', cacheMiddleware(400), (req, res) => require('../models/pub/profile').init(req, res))
 
 module.exports = router
