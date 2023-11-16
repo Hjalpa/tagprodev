e8c345825c2ea96901f8a1b2bfe2994a3e86ecc8
anom <gitlab@hellier.dev>
2023-11-16 11:22:03 +0000

    profile api

--- models/pub/profile.js
+++ models/pub/profile.js
@@ -13,9 +13,23 @@ module.exports.init = async (req, res) => {
 async function getProfileData(profileID) {
 	let raw = await db.select(`
 		SELECT
-			ROUND(openskill::decimal, 2)::real  as openskill
-		FROM tp_player
-		WHERE tp_player.tpid = $1
+			COUNT(*) as games,
+			COUNT(*) filter (WHERE tp_playergame.winner = true) as wins,
+			COUNT(*) filter (WHERE tp_playergame.winner = false) as losses,
+
+			ROUND(COUNT(*) FILTER (WHERE tp_playergame.winner = true) * 100.0 / COUNT(*), 2)::REAL AS winrate,
+
+			SUM(cap_team_for) as CF,
+			SUM(cap_team_against) as CA,
+			SUM(cap_team_for - cap_team_against) as CD,
+			TO_CHAR(SUM(duration) * interval '1 sec', 'hh24:mi:ss') as timeplayed,
+			ROUND(p.openskill::decimal, 2) as openskill
+
+		FROM tp_playergame
+		LEFT JOIN tp_player as p ON p.id = tp_playergame.playerid
+		WHERE p.tpid = $1
+		GROUP BY p.id, p.openskill
+		LIMIT 1
 	`, [profileID], 'row')
 
 	return raw
