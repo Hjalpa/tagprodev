23b1c1ce8f948a997e7f20c5808806fa7072c63c
anom <gitlab@hellier.dev>
2023-11-20 23:46:35 +0000

    test

--- routes/api.js
+++ routes/api.js
@@ -39,6 +39,7 @@ router.post('/pub/import', (req, res) => require('../models/pub/import').import(
 // router.get('/pub/leaderboard', cacheMiddleware(5), (req, res) => require('../models/pub/leaderboard').init(req, res))
 router.get('/pub/home', (req, res) => require('../models/pub/home').init(req, res))
 router.get('/pub/leaderboard', (req, res) => require('../models/pub/leaderboard').init(req, res))
+router.get('/pub/stats', (req, res) => require('../models/pub/stats').init(req, res))
 router.get('/pub/profile/:profileID', (req, res) => require('../models/pub/profile').init(req, res))
 
 module.exports = router
