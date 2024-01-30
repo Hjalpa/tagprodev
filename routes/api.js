84268208f64423a2bfd9a24a78502daee90c5b89
anom <gitlab@hellier.dev>
2024-01-30 22:58:39 +0000

    fix

--- routes/api.js
+++ routes/api.js
@@ -19,7 +19,8 @@ router.post('/pub/import', (req, res) => require('../models/pub/import').import(
 router.get('/pub/home', routeCache.cacheSeconds(60*30), (req, res) => require('../models/pub/home').init(req, res))
 router.get('/pub/leaderboard', routeCache.cacheSeconds(60*30), (req, res) => require('../models/pub/leaderboard').init(req, res))
 router.get('/pub/history/:timezone*', routeCache.cacheSeconds(60*30), (req, res) => require('../models/pub/history').init(req, res))
-router.get('/pub/profile/:profileID*', routeCache.cacheSeconds(60*30), (req, res) => require('../models/pub/profile').init(req, res))
+router.get('/pub/profile/:profileID/:timezone*', routeCache.cacheSeconds(60*30), (req, res) => require('../models/pub/profile').init(req, res))
+//
 router.get('/pub/openskill', (req, res) => require('../models/pub/request').openskill(req, res))
 
 module.exports = router
