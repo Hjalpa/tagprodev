33ce4a3a262461ba5eb4e978799b88cc23c1a89a
anom <gitlab@hellier.dev>
2024-02-09 14:05:06 +0000

    fix disc pen

--- tasks/pubrank_openkill.js
+++ tasks/pubrank_openkill.js
@@ -5,14 +5,17 @@ const db = require('../lib/db')
 init = (() => {})
 init.call = async () => {
 	try {
-		// if(!process.argv[2]) throw 'no gameid'
-		// let gameID = process.argv[2]
-		// await openskill.rank(gameID)
-		let games = await db.select('SELECT * FROM tp_game WHERE id >= 0 ORDER BY datetime ASC', [], 'all')
-		for(let g of games) {
-			console.log(`${g.id}`)
-			await openskill.rank(g.id)
+		if(!process.argv[2]) throw 'no gameid'
+		let gameID = process.argv[2]
+		if(gameID) {
+			console.log(gameID)
+			await openskill.rank(gameID)
 		}
+		// let games = await db.select('SELECT * FROM tp_game WHERE id >= 0 ORDER BY datetime ASC', [], 'all')
+		// for(let g of games) {
+		// 	console.log(`${g.id}`)
+		// 	await openskill.rank(g.id)
+		// }
 	}
 
 	catch(e) {
