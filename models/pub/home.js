7f1211d68cddf20b6c2ae6d60ab73892e5868310
anom <gitlab@hellier.dev>
2024-02-10 12:19:41 +0000

    fix

--- models/pub/home.js
+++ models/pub/home.js
@@ -66,29 +66,22 @@ async function getGames() {
 			) AS blue_team
 
 		FROM tp_game
-
-
-
 		LEFT JOIN tp_map ON tp_map.id = tp_game.mapid
 		LEFT JOIN tp_server ON tp_server.id = tp_game.serverid
-
-        WHERE
-		(
-			(
-				((tp_game.prediction->>'red')::DECIMAL BETWEEN 0 AND 0.15) AND tp_game.winner = 2
-			)
-			OR
-			(
-				((tp_game.prediction->>'blue')::DECIMAL BETWEEN 0 AND 0.15) AND tp_game.winner = 1
-			)
-		)
-
-
 		ORDER BY tp_game.datetime DESC
 		LIMIT 15
 	`, [], 'all')
 
-
+        // WHERE
+		// (
+			// (
+				// ((tp_game.prediction->>'red')::DECIMAL BETWEEN 0 AND 0.25) AND tp_game.winner = 1
+			// )
+			// OR
+			// (
+				// ((tp_game.prediction->>'blue')::DECIMAL BETWEEN 0 AND 0.25) AND tp_game.winner = 2
+			// )
+		// )
 
 	return raw
 }
@@ -122,66 +115,66 @@ async function getPlayerRecentGames(tpid, req) {
 	console.log(`recent games: ${playerName}`)
 
 	let raw = await db.select(`
-	SELECT
-		tp_game.id,
-		tp_game.uuid,
-		tp_game.tpid,
-		tp_game.winner,
-		tp_game.redcaps,
-		tp_game.bluecaps,
-		TO_CHAR(tp_pg.duration * INTERVAL '1 millisecond', 'MI:SS') as duration,
-		tp_game.datetime,
-		tp_game.prediction,
-		tp_map.name as map,
-		tp_server.name as server,
+		SELECT
+			tp_game.id,
+			tp_game.uuid,
+			tp_game.tpid,
+			tp_game.winner,
+			tp_game.redcaps,
+			tp_game.bluecaps,
+			TO_CHAR(tp_pg.duration * INTERVAL '1 millisecond', 'MI:SS') as duration,
+			tp_game.datetime,
+			tp_game.prediction,
+			tp_map.name as map,
+			tp_server.name as server,
 
-		ARRAY(
-			select json_build_object(
-				'name', tp_player.name,
-				'flair', pg.flair,
-				'tpid', tp_player.tpid,
-				'openskill_change', Round(pg.openskill::decimal - COALESCE(xpg.openskill::decimal, 0), 2)::real,
-				'openskill', Round(pg.openskill::decimal, 2)::real,
-				'finished', pg.finished
-			)
-			from tp_playergame as pg
-			left join tp_player on tp_player.id = pg.playerid
-			LEFT JOIN tp_playergame as xpg ON tp_player.id = xpg.playerid AND xpg.datetime = (
-				SELECT max(datetime)
-				FROM tp_playergame
-				WHERE playerid = tp_player.id AND tp_playergame.gameid < pg.gameid
-			)
-			where pg.gameid = tp_game.id and pg.team = 1
-			ORDER BY pg.finished DESC, pg.openskill DESC
-		) AS red_team,
-		ARRAY(
-			select json_build_object(
-				'name', tp_player.name,
-				'flair', pg.flair,
-				'tpid', tp_player.tpid,
-				'openskill_change', Round(pg.openskill::decimal - COALESCE(xpg.openskill::decimal, 0), 2)::real,
-				'openskill', Round(pg.openskill::decimal, 2)::real,
-				'finished', pg.finished
-			)
-			from tp_playergame as pg
-			left join tp_player on tp_player.id = pg.playerid
-			LEFT JOIN tp_playergame as xpg ON tp_player.id = xpg.playerid AND xpg.datetime = (
-				SELECT max(datetime)
-				FROM tp_playergame
-				WHERE playerid = tp_player.id AND tp_playergame.gameid < pg.gameid
-			)
-			where pg.gameid = tp_game.id and pg.team = 2
-			ORDER BY pg.finished DESC, pg.openskill DESC
-		) AS blue_team
+			ARRAY(
+				select json_build_object(
+					'name', tp_player.name,
+					'flair', pg.flair,
+					'tpid', tp_player.tpid,
+					'openskill_change', Round(pg.openskill::decimal - COALESCE(xpg.openskill::decimal, 0), 2)::real,
+					'openskill', Round(pg.openskill::decimal, 2)::real,
+					'finished', pg.finished
+				)
+				from tp_playergame as pg
+				left join tp_player on tp_player.id = pg.playerid
+				LEFT JOIN tp_playergame as xpg ON tp_player.id = xpg.playerid AND xpg.datetime = (
+					SELECT max(datetime)
+					FROM tp_playergame
+					WHERE playerid = tp_player.id AND tp_playergame.gameid < pg.gameid
+				)
+				where pg.gameid = tp_game.id and pg.team = 1
+				ORDER BY pg.finished DESC, pg.openskill DESC
+			) AS red_team,
+			ARRAY(
+				select json_build_object(
+					'name', tp_player.name,
+					'flair', pg.flair,
+					'tpid', tp_player.tpid,
+					'openskill_change', Round(pg.openskill::decimal - COALESCE(xpg.openskill::decimal, 0), 2)::real,
+					'openskill', Round(pg.openskill::decimal, 2)::real,
+					'finished', pg.finished
+				)
+				from tp_playergame as pg
+				left join tp_player on tp_player.id = pg.playerid
+				LEFT JOIN tp_playergame as xpg ON tp_player.id = xpg.playerid AND xpg.datetime = (
+					SELECT max(datetime)
+					FROM tp_playergame
+					WHERE playerid = tp_player.id AND tp_playergame.gameid < pg.gameid
+				)
+				where pg.gameid = tp_game.id and pg.team = 2
+				ORDER BY pg.finished DESC, pg.openskill DESC
+			) AS blue_team
 
-	FROM tp_playergame as tp_pg
-	LEFT JOIN tp_game on tp_pg.gameid = tp_game.id
-	LEFT JOIN tp_player as tp_p ON tp_pg.playerid = tp_p.id
-	LEFT JOIN tp_map ON tp_map.id = tp_game.mapid
-	LEFT JOIN tp_server ON tp_server.id = tp_game.serverid
-	WHERE tp_p.tpid = $1
-	ORDER BY tp_game.datetime DESC
-	LIMIT 15
+		FROM tp_playergame as tp_pg
+		LEFT JOIN tp_game on tp_pg.gameid = tp_game.id
+		LEFT JOIN tp_player as tp_p ON tp_pg.playerid = tp_p.id
+		LEFT JOIN tp_map ON tp_map.id = tp_game.mapid
+		LEFT JOIN tp_server ON tp_server.id = tp_game.serverid
+		WHERE tp_p.tpid = $1
+		ORDER BY tp_game.datetime DESC
+		LIMIT 15
 	`, [tpid], 'all')
 
 	return raw
