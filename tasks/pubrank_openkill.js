df775c639330856b2370531bb3b5a07d5d6f7c7e
anom <gitlab@hellier.dev>
2024-03-12 17:16:24 +0000

    as

--- tasks/pubrank_openkill.js
+++ tasks/pubrank_openkill.js
@@ -5,17 +5,11 @@ const db = require('../lib/db')
 init = (() => {})
 init.call = async () => {
 	try {
-		if(!process.argv[2]) throw 'no gameid'
-		let gameID = process.argv[2]
-		if(gameID) {
-			console.log(gameID)
-			await openskill.rank(gameID)
-		}
-		// let games = await db.select('SELECT * FROM tp_game WHERE id >= 0 ORDER BY datetime ASC', [], 'all')
-		// for(let g of games) {
-		// 	console.log(`${g.id}`)
-		// 	await openskill.rank(g.id)
-		// }
+		 let games = await db.select('SELECT * FROM tp_game WHERE id >= 0 ORDER BY id ASC', [], 'all')
+		 for(let g of games) {
+		 	console.log(`${g.id}`)
+		 	await openskill.rank(g.id)
+		 }
 	}
 
 	catch(e) {
