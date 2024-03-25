1a94310c629f08dc7ede17607e7aacc5d81afdd0
anom <gitlab@hellier.dev>
2024-03-25 10:51:29 +0000

    fix switching

--- tasks/pubrank_openkill.js
+++ tasks/pubrank_openkill.js
@@ -5,7 +5,7 @@ const db = require('../lib/db')
 init = (() => {})
 init.call = async () => {
 	try {
-		 let games = await db.select('SELECT * FROM tp_game WHERE id >= 0 ORDER BY datetime ASC', [], 'all')
+		 let games = await db.select('SELECT * FROM tp_game WHERE id = 78848 ORDER BY datetime ASC', [], 'all')
 		 for(let g of games) {
 		 	console.log(`${g.id}`)
 		 	await openskill.rank(g.id)
