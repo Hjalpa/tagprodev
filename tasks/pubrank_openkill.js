046afa06cfed7af754de12d98c94ba86afc3a42a
anom <gitlab@hellier.dev>
2024-03-30 00:42:15 +0000

    fix

--- tasks/pubrank_openkill.js
+++ tasks/pubrank_openkill.js
@@ -5,7 +5,7 @@ const db = require('../lib/db')
 init = (() => {})
 init.call = async () => {
 	try {
-		 let games = await db.select('SELECT * FROM tp_game WHERE id >= 85000 ORDER BY datetime ASC', [], 'all')
+		 let games = await db.select('SELECT * FROM tp_game WHERE id = 85098 ORDER BY datetime ASC', [], 'all')
 		 for(let g of games) {
 		 	console.log(`${g.id}`)
 		 	await openskill.rank(g.id)
